9994d74832b0ac64d6082d303c69b72c40e2587a
Rafael dos Santos Miguel <97112684+uRaelM@users.noreply.github.com>
2022-04-05 20:26:44 -0300

    Arquivo com variáveis trocadas
    variáveis trocadas

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 from tkinter import *
 
 
+albuns = []
+dados = []
 def arquivo():
     dados = []
 
-    rnome = str(entrada_nome.get())
-    dados.append(rnome)
+    ialbum = str(entrada_album.get())
+    dados.append(ialbum)
+    albuns.append(ialbum)
 
-    rnascimento = str(entrada_nascimento.get())
-    dados.append(rnascimento)
+    ilançamento = str(entrada_ano_lancamento.get())
+    dados.append(ilançamento)
 
-    rcpf = str(entrada_CPF.get())
-    dados.append(rcpf)
+    ibanda_artista = str(entrada_banda_artista.get())
+    dados.append(ibanda_artista)
 
-    rmatricula = str(entrada_matricula.get())
-    dados.append(rmatricula)
+    isim_nao = str(entrada_sim_nao.get())
+    dados.append(isim_nao)
 
     dados_p_salvar = ' | '.join(dados)
     dados_para_salvar = dados_p_salvar + '\n'
     executar = open('arquivo.txt', 'a', encoding='utf-8')
     executar.write(dados_para_salvar)
+    print(albuns)
     executar.close()
-    dados = []
 
 "janela:"
 window = Tk()
-window.title('Trabalho de LNPG')
+window.title('Versão 1.0')
 window.geometry('500x250')
-dados = []
 
 "Labels e Entry:"
-nome = Label(window, text='Nome: ', font=14).pack()
-entrada_nome = Entry(window)
-entrada_nome.pack()
-nascimento = Label(window, text='Nascimento: ', font=14).pack()
-entrada_nascimento = Entry(window)
-entrada_nascimento.pack()
-cpf = Label(window, text='CPF:', font=14).pack()
-entrada_CPF = Entry(window)
-entrada_CPF.pack()
-matricula = Label(window, text='Matricula: ', font=14).pack()
-entrada_matricula = Entry(window)
-entrada_matricula.pack()
+album = Label(window, text='Álbum: ', font=14).pack()
+entrada_album = Entry(window)
+entrada_album.pack()
+ano_lancamento = Label(window, text='Ano de lançamento: ', font=14).pack()
+entrada_ano_lancamento = Entry(window)
+entrada_ano_lancamento.pack()
+banda_artista = Label(window, text='Banda/Artista:', font=14).pack()
+entrada_banda_artista = Entry(window)
+entrada_banda_artista.pack()
+ultimo_album = Label(window, text='Ultimo album de lançamento(Sim/Não):', font=14).pack()
+entrada_sim_nao = Entry(window)
+entrada_sim_nao.pack()
 
 "botão e loop"
-botao_salvar = Button(window, text='Cadastrar', command=arquivo).place(x=220, y=200)
+botao_salvar = Button(window, text='cadastrar', command=arquivo).place(x=150, y=200)
+botao_buscar = Button(window, text='Lista de Álbuns', command=arquivo).place(x=260, y=200)
 window.mainloop()
